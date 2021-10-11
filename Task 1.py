23c60ac4423aea4af777a754fafdb17347b9234a
ImranYagizarov <yagizarov.i@icloud.com>
2021-10-11 18:32:45 +0300

    Lesson 5 HomeWork

---
+++ Task 1.py
@@ -0,0 +1,8 @@
+# Напишите код, моделирующий выпадение поля в рулетке (с учетом поля ZERO)
+
+import numpy as np
+
+for i in range (0,3):
+    a = input()
+    x = int(np.random.uniform(0,36))
+    print (x)
